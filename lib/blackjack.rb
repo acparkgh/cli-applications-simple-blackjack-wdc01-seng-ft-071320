3bc6fad612f9625dc7fd2f710ff5ceb831c1a600
Andrew Park <acpark123@gmail.com>
2020-06-27 19:55:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,8 +43,8 @@ def hit?(initial_round)
   user_input = get_user_input
   
   if user_input == "h"
-     new_card = deal_card
-     card_grand_total = initial_round + new_card
+     
+     card_grand_total = initial_round + deal_card
      
   elsif user_input == "s"
      card_grand_total = initial_round
