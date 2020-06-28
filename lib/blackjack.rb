2f34ff0ff0a89272fd56d7cc082a415cb100d411
Andrew Park <acpark123@gmail.com>
2020-06-28 03:24:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(current_card_total)
   prompt_user
   user_input = get_user_input
     if user_input == "h"
-      current_card_total += deal_card
+      current_card_total = current_card_total + deal_card
     elsif user_input == "s"
       current_card_total
     elsif user_input != "h" || user_input !="s"
@@ -66,7 +66,6 @@ def runner
     until current_card_total > 21 do
       hit?(current_card_total)
       display_card_total(current_card_total)
-      current_card_total
     end
   end_game(current_card_total)
 end
