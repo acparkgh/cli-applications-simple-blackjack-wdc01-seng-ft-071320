09d8ffdf904ee85e101e8ca0b34ddad47cd0774a
Andrew Park <acpark123@gmail.com>
2020-06-28 03:58:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,8 +64,7 @@ def runner
   welcome
   current_card_total = initial_round
     until current_card_total > 21 do
-      p current_card_total
-      hit?(current_card_total)
+      current_card_total = hit?(current_card_total)
       display_card_total(current_card_total)
     end
   end_game(current_card_total)
