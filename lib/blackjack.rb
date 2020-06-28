8b58473c96ecfd694ee93ef7116e94a54e873d33
Andrew Park <acpark123@gmail.com>
2020-06-28 02:13:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,9 +8,9 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(current_card_total)
+def display_card_total(abc)
   # code #display_card_total here
-  puts "Your cards add up to #{current_card_total}"
+  puts "Your cards add up to #{abc}"
 end
 
 def prompt_user
