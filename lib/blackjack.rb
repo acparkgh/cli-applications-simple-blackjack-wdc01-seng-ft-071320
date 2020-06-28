d2c2ac9b7fab27398855cba89927511bdd68d2ce
Andrew Park <acpark123@gmail.com>
2020-06-28 03:53:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,6 +66,7 @@ def runner
     until current_card_total > 21 do
       p current_card_total
       hit?(current_card_total)
+      p current_card_total
       display_card_total(current_card_total)
     end
   end_game(current_card_total)
